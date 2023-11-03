29873b880638f2ae524350674a93b5a2cc8716c2
galdinofabricio <fabricio.isigal@gmail.com>
2023-11-03 18:45:45 -0300

    adicionei mais um item para montar a parte de validação de groupRole

--- src/middleware/ensureAuthenticate.ts
+++ src/middleware/ensureAuthenticate.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import AppError from "../shared/errors/AppErrors";
 import { UserRepository } from "../modules/user/repositore/implemantion/UserRepository";
+import { GroupRoleRepository } from "../modules/role/repositorie/implemantion/GroupRoleRepository";
+import Logger from "../shared/logger/Logger";
 
 interface IPayload {
 	sub: string;
@@ -35,10 +37,26 @@ export async function ensureAuthenticate(
 		const usersVerify = await userRepository.listId({ id: user_id });
 
 		if (!usersVerify) {
+			Logger.warn(
+				`user: ${user_id} tried to authenticate but is not registerede`
+			);
 			throw new AppError("User not exist", 400);
 		}
 
-		request.user = usersVerify;
+		const groupRoleRepository = new GroupRoleRepository();
+		const userGroupRole = await groupRoleRepository.listUserId({ user_id });
+
+		if (!userGroupRole) {
+			Logger.warn(
+				`user: ${user_id} tried to authenticate but did not have a registered rule`
+			);
+			throw new AppError("error authenticating user");
+		}
+
+		request.user = {
+			id: usersVerify.id,
+			role_id: userGroupRole.role_id,
+		};
 
 		next();
 	} catch {
